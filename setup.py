8c8af497a3c4cd203cb11fe48731f675c6865bcf
Patrik Pihlstrom <patrik.pihlstrom@gmail.com>
2019-01-29 23:47:03 +0100

    update setup.py

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ with open('README.md', 'r') as f:
     description = f.read()
 
 setuptools.setup(
-    name='anna',
+    name='libanna',
     version='1.0.0',
     author='Patrik Pihlstrom',
     author_email='patrik.pihlstrom@gmail.com',
     url='https://github.com/patrikpihlstrom/anna',
-    description='simulated & automated testing software',
+    description='simulated & automated website testing software',
     long_description=description,
     long_description_content_type='text/markdown',
     packages=setuptools.find_packages(),
