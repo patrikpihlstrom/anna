c4fc93df09998b061e4ba492f72fce49c3857946
Patrik Pihlstrom <patrik.pihlstrom@gmail.com>
2019-01-29 23:54:52 +0100

    remove unittest from setup.py

--- setup.py
+++ setup.py
@@ -14,7 +14,6 @@ setuptools.setup(
     long_description_content_type='text/markdown',
     packages=setuptools.find_packages(),
     install_requires=[
-        'unittest',
         'selenium'
     ]
 )
