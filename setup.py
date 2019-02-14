7a374ce1d78077785fb8f7d3a6786eff06260d7d
Patrik Pihlström <patrikpihlstrom@users.noreply.github.com>
2019-02-14 21:53:41 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as f:
 
 setuptools.setup(
     name='libanna',
-    version='1.0.4',
+    version='1.0.5',
     author='Patrik Pihlstrom',
     author_email='patrik.pihlstrom@gmail.com',
     url='https://github.com/patrikpihlstrom/anna',
