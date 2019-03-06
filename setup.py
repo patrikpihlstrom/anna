b9ca3a7e995438eb37343a4635ea8b97c9f917b8
Patrik Pihlstrom <patrik.pihlstrom@gmail.com>
2019-03-06 20:31:36 +0100

    update setup.py & add .travis.yml

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ with open('README.md', 'r') as f:
 
 setuptools.setup(
     name='anna_node',
-    version='1.1.4',
+    version='1.1.5',
     author='Patrik Pihlstrom',
     author_email='patrik.pihlstrom@gmail.com',
     url='https://github.com/patrikpihlstrom/anna',
     description='simulated & automated end-to-end website testing software',
     long_description=description,
     long_description_content_type='text/markdown',
-    packages=['anna'],
+    packages=['anna', 'anna.driver'],
     install_requires=[
         'selenium',
         'anna_common',
