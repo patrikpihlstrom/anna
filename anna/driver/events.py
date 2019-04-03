0a8bf42b0952b6716cbee5ffa8e69ebb4ee6bb0e
Patrik Pihlstrom <patrik.pihlstrom@gmail.com>
2019-04-03 23:28:35 +0200

    added xpath selector functionality for '<select><option>...' elements

--- anna/driver/events.py
+++ anna/driver/events.py
@@ -11,35 +11,48 @@ from . import util
 
 def send_keys(driver, event):
 	if wait(driver, event):
-		element = util.get_element(driver, event['target'])
+		if 'target' in event and not isinstance(event['target'], dict):
+			event['target'] = {'type': 'css', 'value': event['target']}
+		element = util.get_element(driver=driver, target=event['target']['value'], type=event['target']['type'])
 		v = event['value'].encode('ascii', 'ignore').decode("utf-8")
 		element.send_keys(v)
 
 
 def submit(driver, event):
 	if wait(driver, event):
-		element = util.get_element(driver, event['target'])
+		if 'target' in event and not isinstance(event['target'], dict):
+			event['target'] = {'type': 'css', 'value': event['target']}
+		element = util.get_element(driver=driver, target=event['target']['value'], type=event['target']['type'])
 		element.submit()
 
 
 def click(driver, event):
 	if wait(driver, event):
-		element = util.get_element(driver, event['target'])
-		action = ActionChains(driver)
-		action.move_to_element(element)
-		action.click(element)
-		action.perform()
+		if 'target' in event and not isinstance(event['target'], dict):
+			event['target'] = {'type': 'css', 'value': event['target']}
+		element = util.get_element(driver=driver, target=event['target']['value'], type=event['target']['type'])
+		if element not in (None, False) and element.tag_name == 'option':
+			element.click()
+		else:
+			action = ActionChains(driver)
+			action.move_to_element(element)
+			action.click(element)
+			action.perform()
 
 
 def hover(driver, event):
 	if wait(driver, event):
-		element = util.get_element(driver, event['target'])
+		if 'target' in event and not isinstance(event['target'], dict):
+			event['target'] = {'type': 'css', 'value': event['target']}
+		element = util.get_element(driver=driver, target=event['target']['value'], type=event['target']['type'])
 		action = ActionChains(driver)
 		action.move_to_element(element)
 		action.perform()
 
 
 def get(driver, event):
+	if 'target' in event and not isinstance(event['target'], dict):
+		event['target'] = {'type': 'css', 'value': event['target']}
 	driver.get(event['target'])
 
 
@@ -50,11 +63,20 @@ def wait(driver, event):
 	:param event:
 	:return:
 	"""
+	if 'target' in event and not isinstance(event['target'], dict):
+		event['target'] = {'type': 'css', 'value': event['target']}
 	try:
 		if event['type'] == 'click':
-			WebDriverWait(driver, 16).until(ec.element_to_be_clickable((By.CSS_SELECTOR, event['target'])))
+			if event['target']['type'] == 'css':
+				WebDriverWait(driver, 16).until(ec.element_to_be_clickable((By.CSS_SELECTOR, event['target']['value'])))
+			elif event['target']['type'] == 'xpath':
+				WebDriverWait(driver, 16).until(ec.element_to_be_clickable((By.XPATH, event['target']['value'])))
 		else:
-			WebDriverWait(driver, 16).until(ec.presence_of_element_located((By.CSS_SELECTOR, event['target'])))
+			if event['target']['type'] == 'css':
+				WebDriverWait(driver, 16).until(
+					ec.presence_of_element_located((By.CSS_SELECTOR, event['target']['value'])))
+			elif event['target']['type'] == 'xpath':
+				WebDriverWait(driver, 16).until(ec.presence_of_element_located((By.XPATH, event['target']['value'])))
 	except TimeoutException as e:
 		if 'required' in event and not event['required']:
 			return False
@@ -68,10 +90,14 @@ def sleep(driver, event):
 
 
 def switch_to(driver, event):
+	if 'target' in event and not isinstance(event['target'], dict):
+		event['target'] = {'type': 'css', 'value': event['target']}
 	scroll_to(driver, event)
-	element = util.get_element(driver, event['target'])
+	element = util.get_element(driver=driver, target=event['target']['value'], type=event['target']['type'])
 	driver.switch_to.frame(element)
 
 
 def scroll_to(driver, event):
-	driver.execute_script('arguments[0].scrollIntoView(true);', util.get_element(driver, event['target']))
+	if 'target' in event and not isinstance(event['target'], dict):
+		event['target'] = {'type': 'css', 'value': event['target']}
+	driver.execute_script('arguments[0].scrollIntoView(true);', util.get_element(driver=driver, target=event['target']['value'], type=event['target']['type']))
--- tests/unit/firefox/test_events.py
+++ tests/unit/firefox/test_events.py
@@ -17,103 +17,89 @@ class TestEvents(unittest.TestCase):
 
 	def test_click(self):
 		self.driver.get('http://annahub.se:8000/test/')
-		for site in self.options['sites']:
-			task = Task().load_from_test_module('click')
-			task.execute_events(self.driver, events)
-			self.assertTrue(task.check(self.driver, assertions))
+		task = Task().load_from_test_module('click')
+		task.execute_events(self.driver, events)
+		self.assertTrue(task.check(self.driver, assertions))
 
 	def test_current_url(self):
 		self.driver.get('http://annahub.se:8000/test/')
-		for site in self.options['sites']:
-			task = Task().load_from_test_module('current_url')
-			task.execute_events(self.driver, events)
-			self.assertTrue(task.check(self.driver, assertions))
+		task = Task().load_from_test_module('current_url')
+		task.execute_events(self.driver, events)
+		self.assertTrue(task.check(self.driver, assertions))
 
 	def test_send_keys(self):
 		self.driver.get('http://annahub.se:8000/test/')
-		for site in self.options['sites']:
-			task = Task().load_from_test_module('send_keys')
-			task.execute_events(self.driver, events)
-			self.assertTrue(task.check(self.driver, assertions))
+		task = Task().load_from_test_module('send_keys')
+		task.execute_events(self.driver, events)
+		self.assertTrue(task.check(self.driver, assertions))
 
 	def test_submit(self):
 		self.driver.get('http://annahub.se:8000/test/')
-		for site in self.options['sites']:
-			task = Task().load_from_test_module('submit')
-			task.execute_events(self.driver, events)
-			self.assertTrue(task.check(self.driver, assertions))
+		task = Task().load_from_test_module('submit')
+		task.execute_events(self.driver, events)
+		self.assertTrue(task.check(self.driver, assertions))
 
 	def test_switch_to(self):
 		self.driver.get('http://annahub.se:8000/test/')
-		for site in self.options['sites']:
-			task = Task().load_from_test_module('switch_to')
-			task.execute_events(self.driver, events)
-			self.assertTrue(task.check(self.driver, assertions))
+		task = Task().load_from_test_module('switch_to')
+		task.execute_events(self.driver, events)
+		self.assertTrue(task.check(self.driver, assertions))
 
 	def test_hover(self):
 		self.driver.get('http://annahub.se:8000/test/')
-		for site in self.options['sites']:
-			task = Task().load_from_test_module('hover')
-			task.execute_events(self.driver, events)
-			self.assertTrue(task.check(self.driver, assertions))
+		task = Task().load_from_test_module('hover')
+		task.execute_events(self.driver, events)
+		self.assertTrue(task.check(self.driver, assertions))
 
 	def test_wait(self):
 		self.driver.get('http://annahub.se:8000/test/')
-		for site in self.options['sites']:
-			task = Task().load_from_test_module('wait')
-			task.execute_events(self.driver, events)
-			self.assertTrue(task.check(self.driver, assertions))
+		task = Task().load_from_test_module('wait')
+		task.execute_events(self.driver, events)
+		self.assertTrue(task.check(self.driver, assertions))
 
 	def test_fail(self):
 		self.driver.get('http://annahub.se:8000/test/')
-		for site in self.options['sites']:
-			task = Task().load_from_test_module('fail')
-			try:
-				task.execute_events(self.driver, events)
-				result = task.check(self.driver, assertions)
-				self.assertFalse(result)
-			except common.exceptions.NoSuchElementException:
-				pass
+		task = Task().load_from_test_module('fail')
+		try:
+			task.execute_events(self.driver, events)
+			result = task.check(self.driver, assertions)
+			self.assertFalse(result)
+		except common.exceptions.NoSuchElementException:
+			pass
 
 	def test_iframe_click(self):
 		self.test_switch_to()
-		for site in self.options['sites']:
-			task = Task().load_from_test_module('iframe.click')
-			task.execute_events(self.driver, events)
-			self.assertTrue(task.check(self.driver, assertions))
+		task = Task().load_from_test_module('iframe.click')
+		task.execute_events(self.driver, events)
+		self.assertTrue(task.check(self.driver, assertions))
 
 	def test_iframe_send_keys(self):
 		self.test_switch_to()
-		for site in self.options['sites']:
-			task = Task().load_from_test_module('iframe.send_keys')
-			task.execute_events(self.driver, events)
-			self.assertTrue(task.check(self.driver, assertions))
+		task = Task().load_from_test_module('iframe.send_keys')
+		task.execute_events(self.driver, events)
+		self.assertTrue(task.check(self.driver, assertions))
 
 	def test_iframe_submit(self):
 		self.test_switch_to()
-		for site in self.options['sites']:
-			task = Task().load_from_test_module('iframe.submit')
-			task.execute_events(self.driver, events)
-			self.assertTrue(task.check(self.driver, assertions))
+		task = Task().load_from_test_module('iframe.submit')
+		task.execute_events(self.driver, events)
+		self.assertTrue(task.check(self.driver, assertions))
 
 	def test_iframe_hover(self):
 		self.test_switch_to()
-		for site in self.options['sites']:
-			task = Task().load_from_test_module('iframe.hover')
-			task.execute_events(self.driver, events)
-			self.assertTrue(task.check(self.driver, assertions))
+		task = Task().load_from_test_module('iframe.hover')
+		task.execute_events(self.driver, events)
+		self.assertTrue(task.check(self.driver, assertions))
 
 	def test_iframe_wait(self):
 		self.test_switch_to()
-		for site in self.options['sites']:
-			task = Task().load_from_test_module('iframe.wait')
-			task.execute_events(self.driver, events)
-			self.assertTrue(task.check(self.driver, assertions))
+		task = Task().load_from_test_module('iframe.wait')
+		task.execute_events(self.driver, events)
+		self.assertTrue(task.check(self.driver, assertions))
 
 	def test_required_event(self):
-		for site in self.options['sites']:
-			task = Task().load_from_test_module('required.true')
-			with self.assertRaises(common.exceptions.TimeoutException):
-				task.execute_events(self.driver, events)
-			task = Task().load_from_test_module('required.false')
+		task = Task().load_from_test_module('required.true')
+		with self.assertRaises(common.exceptions.TimeoutException):
 			task.execute_events(self.driver, events)
+		task = Task().load_from_test_module('required.false')
+		task.execute_events(self.driver, events)
